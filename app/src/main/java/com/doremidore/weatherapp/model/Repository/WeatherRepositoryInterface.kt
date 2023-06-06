985be304217b989bba7de851a9808a2cf735945a
doreMIDore <Igor12341999@gmail.com>
2023-06-06 17:52:23 +0300

    Renamed fun processWeatherData() Passing Resources instead Context Adding comments

--- app/src/main/java/com/doremidore/weatherapp/model/Repository/WeatherRepositoryInterface.kt
+++ app/src/main/java/com/doremidore/weatherapp/model/Repository/WeatherRepositoryInterface.kt
@@ -4,7 +4,7 @@ import android.graphics.drawable.PictureDrawable
 import com.doremidore.weatherapp.model.WeatherModel
 
 interface WeatherRepositoryInterface {
-    suspend fun getWeatherData(lan: String = "55.751244", lon: String = "37.618423"): WeatherModel
+    suspend fun getWeatherData(lat: String = "55.751244", lon: String = "37.618423"): WeatherModel
     suspend fun getImage(iconPath: String): PictureDrawable
     fun getData() : WeatherModel?
 }
